669c0932b530213cb4bc17e38613b4d3c04d92af
radziu2402 <radzi2002@wp.pl>
2023-03-08 00:32:00 +0100

    small changes in list

--- DoublyLinkedList.cpp
+++ DoublyLinkedList.cpp
@@ -3,3 +3,49 @@
 //
 
 #include "DoublyLinkedList.h"
+
+DoublyLinkedList::DoublyLinkedList() {
+    head = nullptr;
+    tail = nullptr;
+    size = 0;
+}
+
+DoublyLinkedList::~DoublyLinkedList() {
+    while (head != tail) {
+        head = head->next;
+        delete head->prev;
+    }
+    delete tail;
+}
+
+void DoublyLinkedList::insertHead(int value) {
+
+}
+
+void DoublyLinkedList::insertTail(int value) {
+
+}
+
+void DoublyLinkedList::insert(int value, int index) {
+
+}
+
+void DoublyLinkedList::removeHead() {
+
+}
+
+void DoublyLinkedList::removeTail() {
+
+}
+
+void DoublyLinkedList::remove(int index) {
+
+}
+
+void DoublyLinkedList::displayList() {
+
+}
+
+bool DoublyLinkedList::checkIfExist(int value) {
+    return false;
+}
